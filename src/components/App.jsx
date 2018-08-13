9eda92ef6af5ebe311f93142c2020865b95ed97e
rvd2pwipwip <herve_desrosiers@hotmail.com>
2018-08-12 21:15:51 -0400

    fake server data into intial App state through constructor, fake server delay and ternary to render React.Fragment or Loading... message

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,12 +8,36 @@ const defaultStyle = {
   color: "#fff"
 };
 
+let fakeServerData = {
+  user: {
+    name: "Herve",
+    playlists: [
+      {
+        name: "rvSpanoushtre",
+        songs: ["1977", "Tren al Sur", "Nada"]
+      },
+      {
+        name: "rvChicaSpanoushtre",
+        songs: ["Amor Sin Fin", "Flaca", "Padre Nuestro"]
+      },
+      {
+        name: "rvChom",
+        songs: ["Hells Bells", "Whole Lotta Love", "Godzilla"]
+      },
+      {
+        name: "rvAltern",
+        songs: ["In a Big Country", "Nostalgia", "The Unguarded Moment"]
+      }
+    ]
+  }
+};
+
 class Aggregate extends Component {
   state = {};
   render() {
     return (
       <div style={{ ...defaultStyle, width: "40%", display: "inline-block" }}>
-        <h2>Number Title</h2>
+        <h2>{this.props.playlists && this.props.playlists.length} Title</h2>
       </div>
     );
   }
@@ -55,16 +79,46 @@ class Playlist extends Component {
 }
 
 class App extends Component {
+  //set initial state before component did mount in the constructor
+  constructor(props) {
+    super(props);
+    this.state = { serverData: {} };
+  }
+
+  componentDidMount() {
+    //fake server delay
+    setTimeout(() => {
+      this.setState({ serverData: fakeServerData });
+    }, 1000);
+  }
+
   render() {
     return (
       <div className="App">
-        <h1 style={{ ...defaultStyle, "font-size": "54px" }}>Title</h1>
-        <Aggregate />
-        <Aggregate />
-        <Filter />
-        <Playlist />
-        <Playlist />
-        <Playlist />
+        {/* check if there is user data in serverData before rendering React.Fragment*/}
+        {this.state.serverData.user ? (
+          <React.Fragment>
+            <h1
+              style={{
+                ...defaultStyle,
+                "font-size": "54px",
+                "font-weight": "100"
+              }}
+            >
+              {this.state.serverData.user.name}
+              's Playlists
+            </h1>
+            <Aggregate playlists={this.state.serverData.user.playlists} />
+            <Aggregate />
+            <Filter />
+            <Playlist />
+            <Playlist />
+            <Playlist />
+          </React.Fragment>
+        ) : (
+          // if there was no user data at start of ternary, render h3 Loading... instead
+          <h3 style={defaultStyle}>Loading...</h3>
+        )}
       </div>
     );
   }
